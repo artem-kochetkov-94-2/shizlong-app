409ce5ffeebf5c429547870094e74a0b92d01cda
unknown <evgeniy.popov.jsx@gmail.com>
2025-03-05 17:26:58 +0300

    added PaymentMethodsPage

--- src/presentation/pages/PaymentMethods/PaymentMethods.tsx
+++ src/presentation/pages/PaymentMethods/PaymentMethods.tsx
@@ -1,3 +1,24 @@
+import { PageHeader } from '@src/presentation/ui-kit/PageHeader';
+import styles from './PaymentsMethods.module.css';
+import { Button } from '@src/presentation/ui-kit/Button';
+import { Icon } from '@src/presentation/ui-kit/Icon';
+
 export const PaymentMethods = () => {
-  return <>Способы оплаты</>;
+  return (
+    <>
+      <PageHeader>Способы оплаты</PageHeader>
+      <div className={styles.card}>
+        <div className={styles.header}>
+          <div className={styles.circle} />
+          <div className={styles.text}>На банковскую карту</div>
+          <Icon name={'check4'} size={'medium'} />
+        </div>
+        <div className={styles.empty}>
+          <div className={styles.info}>Вы ещё не добавили ни одной карты</div>
+          <Icon name={'card'} size={'large'} className={styles.icon} />
+        </div>
+        <Button variant={'yellow'}>Добавить карту</Button>
+      </div>
+    </>
+  );
 };
