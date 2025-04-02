8966413a8ef85c000a6e6661414d837aa10be754
unknown <evgeniy.popov.jsx@gmail.com>
2025-04-02 15:34:49 +0300

    Вынес инпут в отдельный компонент, настроил логику в authModal

--- src/presentation/components/ModalAuth/ModalAuth.tsx
+++ src/presentation/components/ModalAuth/ModalAuth.tsx
@@ -2,25 +2,23 @@ import PhoneInput from 'react-phone-input-2';
 import { useState } from 'react';
 import { Button } from '@src/presentation/ui-kit/Button';
 import { verificationStore } from '@src/application/store/verificationStore';
-import { useNavigate } from 'react-router-dom';
-import { Routes } from '@src/routes';
-import styles from './ModalAuth.module.css';
 import { Icon } from '@src/presentation/ui-kit/Icon';
+import { VerificationInput } from '../VerificationInput';
+import styles from './ModalAuth.module.css';
 
 interface ModalProps {
   onClose: () => void;
 }
 
 export const ModalAuth = ({ onClose }: ModalProps) => {
-  const navigate = useNavigate();
   const [value, setValue] = useState('');
+  const [isVerificationStarted, setIsVerificationStarted] = useState(false);
 
   const submit = () => {
     verificationStore.reset();
     verificationStore.setPhoneNumber(value);
     verificationStore.startVerification();
-    onClose();
-    navigate(Routes.Verification);
+    setIsVerificationStarted(true);
   };
 
   return (
@@ -36,25 +34,31 @@ export const ModalAuth = ({ onClose }: ModalProps) => {
           Пожалуйста, авторизуйтесь, для дальнейшего использования приложения
         </div>
         <div className={styles.content}>
-          <div className={styles.h2}>Авторизация</div>
-          <div className={styles.p}>Введите свой номер телефона</div>
-          <PhoneInput
-            country={'ru'}
-            value={value}
-            onChange={(v) => setValue(v)}
-            specialLabel={'Номер телефона'}
-            placeholder={'000 000 0000'}
-            containerClass={styles.input}
-          />
-          <Button
-            variant='primary'
-            size='large'
-            disabled={value.length !== 11}
-            withShadow={true}
-            onClick={submit}
-          >
-            Авторизация
-          </Button>
+          {isVerificationStarted ? (
+            <VerificationInput onExit={onClose} showNavigateBtn={false} />
+          ) : (
+            <>
+              <div className={styles.h2}>Авторизация</div>
+              <div className={styles.p}>Введите свой номер телефона</div>
+              <PhoneInput
+                country={'ru'}
+                value={value}
+                onChange={(v) => setValue(v)}
+                specialLabel={'Номер телефона'}
+                placeholder={'000 000 0000'}
+                containerClass={styles.input}
+              />
+              <Button
+                variant='primary'
+                size='large'
+                disabled={value.length !== 11}
+                withShadow={true}
+                onClick={submit}
+              >
+                Авторизация
+              </Button>
+            </>
+          )}
           <div className={styles.hint}>
             Нажимая «Войти», вы соглашаетесь с <a href='#'>Условиями использования</a> и{' '}
             <a href='#'>Политикой конфиденциальности</a>
