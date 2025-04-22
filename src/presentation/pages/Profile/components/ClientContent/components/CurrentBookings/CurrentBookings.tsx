33ab24272a76271a97216606a21b4b346bd1a858
unknown <evgeniy.popov.jsx@gmail.com>
2025-04-22 20:17:06 +0300

    added virtuoso scroll

--- src/presentation/pages/Profile/components/ClientContent/components/CurrentBookings/CurrentBookings.tsx
+++ src/presentation/pages/Profile/components/ClientContent/components/CurrentBookings/CurrentBookings.tsx
@@ -1,16 +1,23 @@
-import { observer } from "mobx-react-lite";
-import { bookingsStore } from "@src/application/store/bookingsStore";
-import styles from "./CurrentBookings.module.css";
-import { BookingCard } from "@src/presentation/components/BookingCard";
+import { observer } from 'mobx-react-lite';
+import { bookingsStore } from '@src/application/store/bookingsStore';
+import styles from './CurrentBookings.module.css';
+import { BookingCard } from '@src/presentation/components/BookingCard';
+import { Virtuoso } from 'react-virtuoso';
 
 export const CurrentBookings = observer(() => {
-    const { currentBookings } = bookingsStore;
+  const { currentBookings } = bookingsStore;
 
-    return (
-        <div className={styles.container}>
-            {currentBookings.map((booking) => (
-                <BookingCard key={booking.id} booking={booking} />
-            ))}
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Virtuoso
+        style={{ flex: 1 }}
+        className={styles.virtuoso}
+        totalCount={currentBookings.length}
+        itemContent={(index) => {
+          const booking = currentBookings[index];
+          return <BookingCard key={booking.id} booking={booking} />;
+        }}
+      />
+    </div>
+  );
 });
